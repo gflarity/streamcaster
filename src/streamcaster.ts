2562f9dc10fa957f215bee020c33a3d9623f6a87
Geoffrey Flarity <geoff.flarity@gmail.com>
2021-03-22 15:27:05 -0400

    start using video.js

--- src/streamcaster.ts
+++ src/streamcaster.ts
@@ -1,21 +1,29 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 const router = new Router()
+/*
 router.get('/', async ctx => {
     ctx.response.body = await Deno.readTextFile('../static/index.html')
     ctx.response.type = 'html'
 })
+
 router.get('/video', async ctx => {
     ctx.response.headers.set("Content-Type", 'video/mp4')
 
     // this tells video players that they can jump around
     ctx.response.headers.set("Accept-Ranges", "bytes")
+ 
     ctx.response.body = await Deno.readFile('../static/BigBuckBunny.mp4')
 })
+*/
 
 app.use(async (ctx, next) => {
-    await next();
+    ctx.response.headers.set("Accept-Ranges", "bytes")
+    await send(ctx, ctx.request.url.pathname, {
+        root: `${Deno.cwd()}/../static`,
+        index: "index.html",
+      });
 })
 
 app.use(router.routes())
